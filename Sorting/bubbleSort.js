9edcd651f89b3c7d84568ab2d2908fb92f80e4f7
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-08-22 11:00:24 +0530

    optimize bubble sort

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,10 +1,14 @@
 const bubbleSort = arr => {
+	let noSwap;
 	for (let i = 0; i < arr.length; i++) {
+		noSwap = true;
 		for (let j = 0; j < arr.length - i - 1; j++) {
 			if (arr[j] > arr[j + 1]) {
+				noSwap = false;
 				[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
 			}
 		}
+		if (noSwap) break;
 	}
 };
 
